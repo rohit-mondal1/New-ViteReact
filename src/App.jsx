566268317dc0f7e58d19aed1bf53a3960f00afb3
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-04-20 22:27:46 +0530

    fast commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Rohit</h1>
+        <button className="btn">Button</button>
+        <button className="btn btn-primary">Button</button>
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-accent">Button</button>
+        <button className="btn btn-ghost">Button</button>
+        <button className="btn btn-link">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
